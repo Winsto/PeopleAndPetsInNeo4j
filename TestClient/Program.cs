9dba587ce3a2cb22b0b2866463b685936bd4b8b6
winsto <winsto@live.co.uk>
2014-10-29 01:48:00 +0000

    Add a simple console test client

---
+++ TestClient/Program.cs
@@ -0,0 +1,38 @@
+﻿
+namespace TestClient
+{
+    using DomainModel;
+    using Neo4jPersistence;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dogs = new Animal(familyName: "Canidae", trivialName: "dog");
+
+            var cats = new Animal(familyName: "Felidae", trivialName: "cat");
+
+            var cattle = new Animal(familyName: "Bovidae", trivialName: "cattle");
+
+            var bandicoots = new Animal(familyName: "Peramelidae", trivialName: "bandicoots");
+
+            var crashBanidcoot = new Pet(name: "Crash", whatAnimalItIs: bandicoots);
+
+            var adam = new Person("Adam");
+
+            var steve = new Person("Steve");
+
+            adam.CaresFor(crashBanidcoot);
+
+            steve.CaresFor(new Pet(name:"Rex", whatAnimalItIs:dogs));
+
+            adam.CaresFor(new Pet(name:"Rex", whatAnimalItIs:dogs));
+
+            var providesMappers = new SimpleContainer();
+            
+            providesMappers.ResolvePersonMapper().SaveItem(adam);
+
+            providesMappers.ResolvePersonMapper().SaveItem(steve);
+        }
+    }
+}
---
+++ TestClient/SimpleContainer.cs
@@ -0,0 +1,41 @@
+﻿namespace TestClient
+{
+    using DomainModel;
+    using Neo4jPersistence;
+
+    public sealed class SimpleContainer
+    {
+        private AnimalMapper mapsAnimals;
+        private PetMapper mapsPets;
+        private PersonMapper mapsPersons;
+
+        public SimpleContainer()
+        {
+            RegisterDependencies();
+        }
+
+        private void RegisterDependencies()
+        {
+            mapsAnimals = new AnimalMapper();
+
+            mapsPets = new PetMapper(animalMapper: mapsAnimals);
+
+            mapsPersons = new PersonMapper(petMapper: mapsPets);
+        }
+
+        public AnimalMapper ResolveAnimalMapper()
+        {
+            return mapsAnimals;
+        }
+
+        public PetMapper ResolvePetMapper()
+        {
+            return mapsPets;
+        }
+
+        public PersonMapper ResolvePersonMapper()
+        {
+            return mapsPersons;
+        }
+    }
+}
