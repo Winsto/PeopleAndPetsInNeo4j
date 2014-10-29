1bc149c2c7864d94f94fedf2a6e3c1be4021e99d
winsto <winsto@live.co.uk>
2014-10-29 01:01:47 +0000

    Remove junk from file; should have been done before previous commit

--- Neo4jPersistence/PetMapper.cs
+++ Neo4jPersistence/PetMapper.cs
@@ -30,11 +30,6 @@
                 WithParam("petName",itemToSave.Name).
                 ExecuteWithoutResults();
 
-            //var matchParameters = new Dictionary<string, object>(2);
-
-            //matchParameters["petName"] = itemToSave.Name;
-            //matchParameters["animalTrivialName"] = itemToSave.WhatAnimalIsIt.TrivialName;
-
             databaseConnection.Cypher.
                 Match("(pet:Pet), (animalFamily:Animal)").
                 Where<Pet>(pet => pet.Name == itemToSave.Name).
@@ -43,13 +38,6 @@
                 ExecuteWithoutResults();
 
 
-//             db.Cypher.
-//* Match("(pet:Pet), (animalFamily:Animal)").
-//* Where<Pet>(pet => pet.Name == "Crash").
-//* AndWhere<Animal>(animalFamily => animalFamily.TrivialName == "bandicoots").
-//* Merge("(pet)-[:IsMemberOf]->(animalFamily)").
-//* ExecuteWithoutResults();
-//>
         }
     }
 }
